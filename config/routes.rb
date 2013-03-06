692a300afa6eb0ba11e1da658c882d6fa0e5b72a
Brad Olstad <bradolstad@gmail.com>
2013-03-06 16:11:00 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Wwii::Application.routes.draw do
 
-<<<<<<< HEAD
-  resources :campaigns
-=======
-  resources :events
-
-
-  resources :countries
-
-
-  resources :units
-
-
-  root :to => 'Campaigns#index'
->>>>>>> 72bf35c6f514cac65902391c13fb3d0a9d644fb6
-
-
   root :to => 'Campaigns#index'
 
   resources :operations
