b5765db95fe579ccee9a8b25eabe7fd4271e7719
Brad Olstad <bradolstad@gmail.com>
2013-03-15 17:20:45 -0500

    bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
       images = @wiki.image_urls.shuffle
       h = @wiki.sanitized_content
       images.each do |image|
-        num = rand(1..6)
+        num = (1..6).to_a.sample
         if num==1
           h.sub!("<p>","<p class=\"clearfix\"><img class=\"img-polaroid\" src=\"#{image}\" width=150 align=\"left\")\">")
         elsif num==2
