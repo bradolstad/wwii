7e0ac3214d421c1b9d3892b076e0e0af7471eac7
Brad Olstad <bradolstad@gmail.com>
2013-03-18 13:53:49 -0500

    bug fixes

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -13,12 +13,18 @@ class Wiki
         if h.scan(/==\s*Notes\s*==/).count > 0
           h = h.split(/==\s*Notes\s*==/).reverse.pop
         end
+        if h.scan(/==\s*References\s*==/).count > 0
+          h = h.split(/==\s*References\s*==/).reverse.pop
+        end
+        if h.scan(/==\s*Footnotes\s*==/).count > 0
+          h = h.split(/==\s*Footnotes\s*==/).reverse.pop
+        end
         h.gsub!(/====\s*(.+)\s*====/,'<i>\1</i>')
         h.gsub!(/===\s*(.+)\s*===/,'<b>\1</b>')
-        h.sub!(/<p>\s*==(.+)\s*==/,'<a class="main-list-link" id="wiki_div_link_0" data-toggle="collapse" data-target="#wiki_div_0">\1</a><div id="wiki_div_0" class="collapse"><p>')
+        h.sub!(/<p>\s*==(.+)\s*==/,'<a class="main-list main-list-link" id="wiki_div_link_0" data-toggle="collapse" data-target="#wiki_div_0">\1</a><div id="wiki_div_0" class="collapse"><p>')
         index = 1
         while index < topics do
-          h.sub!(/<p>==\s*(.+)\s*==/,'</p></div><a class="main-list-link" id="wiki_div_link_'+ index.to_s + '" data-toggle="collapse" data-target="#wiki_div_'+index.to_s+'">\1</a><div id="wiki_div_'+index.to_s+'" class="collapse"><p>')
+          h.sub!(/<p>==\s*(.+)\s*==/,'</p></div><a class="main-list main-list-link" id="wiki_div_link_'+ index.to_s + '" data-toggle="collapse" data-target="#wiki_div_'+index.to_s+'">\1</a><div id="wiki_div_'+index.to_s+'" class="collapse"><p>')
           index += 1
         end
         h = h + "</p></div>"
